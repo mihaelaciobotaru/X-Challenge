474c0d7d89c026b5a0e0861ad2d589e6408a451b
Mihaela Ciobotaru <mihaela.gabriela.93@gmail.com>
2016-04-10 23:16:37 +0300

    add questions;unfixed checkbox

--- src/AppBundle/Controller/QuestionController.php
+++ src/AppBundle/Controller/QuestionController.php
@@ -3,8 +3,66 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\QuestionType;
+use AppBundle\Entity\Question;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class QuestionController extends Controller
 {
+    public function editAction(Request $request, $cid = 1, $id = -1)
+    {
+        $sc = $this->get('security.authorization_checker');
+        if (!$sc->isGranted("ROLE_MASTER")) {
+            throw new AccessDeniedException();
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $category = $em->getRepository("AppBundle:Category")->find($cid);
+
+        if ($category != null) {
+            $question = $em->getRepository("AppBundle:Question")->find($id);
+
+            if ($question != null) {
+                $action = "edit";
+                $title = "Editeaza intrebare";
+            } else {
+                $action = "create";
+                $title = "Adauga intrebare";
+                $question = new Question();
+            }
+            $form = $this->createForm(QuestionType::class, $question);
+            if ($request->getMethod() == "POST") {
+                $form->handleRequest($request);
+
+                if ($form->isValid()) {
+                    $answers = array();
+
+                    for ($i = 1; $i<=5; $i++) {
+                        if ($form->has("answer".$i) && !empty($form->get("answer".$i)->getData())) {
+                            $answers[$form->get("answer".$i)->getData()] = $form->get("check".$i)->getData();
+                        }
+                    }
+                    $question->setAnswerList(json_encode($answers));
+                    if ($action == "create") {
+                        $user = $this->get('security.token_storage')->getToken()->getUser();
+                        $question->setUser($user);
+                        $question->setCategory($category);
+                    }
+                    $em->persist($question);
+                    $em->flush();
+                } else {
+                    //toastr::form is not valid
+                }
+            }
+        } else {
+            //toastr::category does not exist
+        }
+
+        return $this->render("AppBundle:Question:edit.html.twig", array(
+           "title" => $title,
+            "form" => $form->createView(),
+        ));
+
+    }
 }
---
+++ src/AppBundle/Form/QuestionType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class QuestionType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('text', TextType::Class, array("attr" => array("class" => "form-control"), "label"=>"Continut",
+                                                'label_attr' => array('class' => 'col-sm-2 control-label')))
+            ->add('score', IntegerType::Class, array("attr" => array("class" => "form-control"), "label" => "Punctaj",
+                                                    'label_attr' => array('class' => 'col-sm-2 control-label')))
+            ->add('answer1', TextAreaType::Class, array("attr" => array("class" => "form-control"), "label" => "Raspuns 1",
+                                                    'label_attr' => array('class' => 'col-sm-2 control-label'), "mapped" => false))
+            ->add('check1', CheckboxType::Class, array("mapped" => false, "required" => false))
+            ->add('answer2', TextAreaType::Class, array("attr" => array("class" => "form-control"), "label" => "Raspuns 2",
+                                                    'label_attr' => array('class' => 'col-sm-2 control-label'), "mapped" => false))
+            ->add('check2', CheckboxType::Class, array("mapped" => false, "required" => false))
+            ->add('answer3', TextAreaType::Class, array("attr" => array("class" => "form-control"), "label" => "Raspuns 3",
+                                                'label_attr' => array('class' => 'col-sm-2 control-label'), "mapped" => false))
+            ->add('check3', CheckboxType::Class, array("mapped" => false, "required" => false))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Question'
+        ));
+    }
+}
